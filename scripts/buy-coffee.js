a239da5db8dfe7284ec52f05fcdf5f1f14fe2894
Ishaq Nasir <ishaqnasir47@gmail.com>
2022-10-08 03:21:39 +0100

    crack

--- scripts/buy-coffee.js
+++ scripts/buy-coffee.js
@@ -1,72 +1,71 @@
 const hre = require("hardhat");
 
-//returns Ethers of the given address
+// Returns the Ether balance of a given address.
 async function getBalance(address) {
   const balanceBigInt = await hre.ethers.provider.getBalance(address);
   return hre.ethers.utils.formatEther(balanceBigInt);
 }
 
-//logs the Ether balances for a list of addresses 
+// Logs the Ether balances for a list of addresses.
 async function printBalances(addresses) {
   let idx = 0;
-  for(const address of addresses) {
-    console.log(`Address ${idx} balance:`, await getBalance(address));
-    idx++;
+  for (const address of addresses) {
+    console.log(`Address ${idx} balance: `, await getBalance(address));
+    idx ++;
   }
 }
 
-//Logs the memos stored on chain from coffee purchases.
-
+// Logs the memos stored on-chain from coffee purchases.
 async function printMemos(memos) {
-  for(const memo of memos) {
+  for (const memo of memos) {
     const timestamp = memo.timestamp;
-    const tipper = tipper.name;
+    const tipper = memo.name;
     const tipperAddress = memo.from;
     const message = memo.message;
     console.log(`At ${timestamp}, ${tipper} (${tipperAddress}) said: "${message}"`);
   }
 }
+
 async function main() {
-  //Get the example accounts we'll be working on
+  // Get the example accounts we'll be working with.
   const [owner, tipper, tipper2, tipper3] = await hre.ethers.getSigners();
 
-  //We give the contract to deploy 
+  // We get the contract to deploy.
   const BuyMeCoffee = await hre.ethers.getContractFactory("BuyMeCoffee");
   const buyMeCoffee = await BuyMeCoffee.deploy();
 
-  //deploy contract 
+  // Deploy the contract.
   await buyMeCoffee.deployed();
-  console.log("BuyMeACoffee deployed to :", buyMeCoffee.address);
+  console.log("BuyMeCoffee deployed to:", buyMeCoffee.address);
 
-  //check balances before the coffee purchase
+  // Check balances before the coffee purchase.
   const addresses = [owner.address, tipper.address, buyMeCoffee.address];
-  console.log("=== start ===");
+  console.log("== start ==");
   await printBalances(addresses);
 
-  //Buy owner a few coffee
+  // Buy the owner a few coffees.
   const tip = {value: hre.ethers.utils.parseEther("1")};
-  await buyMeCoffee.connect(tipper).buyCoffee("Umar", "You are awesome and i appreciate it all", tip);
+  await buyMeCoffee.connect(tipper).buyCoffee("Umar", "You are awesome and i appreciate it all!", tip);
   await buyMeCoffee.connect(tipper2).buyCoffee("Akintunde Ismaheel", "I love your steadfastness and i appreciate all ya effort", tip);
-  await buyMeCoffee.connect(tipper3).buyCoffee("Ayomide Fadoro", "I wish you would learn this thing, it is going to berewarding", tip);
+  await buyMeCoffee.connect(tipper3).buyCoffee("Ayomide Fadoro", "I wish you would learn this thing, it is going to be rewarding", tip);
 
-  //check the balance of the coffee purchase
-  console.log("=== bought coffee ===");
+  // Check balances after the coffee purchase.
+  console.log("== bought coffee ==");
   await printBalances(addresses);
 
-  //withdraw 
+  // Withdraw.
   await buyMeCoffee.connect(owner).withdrawTips();
 
-  //check balances after withdrawal 
-  console.log("=== withdrawTips ===");
+  // Check balances after withdrawal.
+  console.log("== withdrawTips ==");
   await printBalances(addresses);
 
-  //checkout Memos
-  console.log("=== Memos ===");
+  // Check out the memos.
+  console.log("== memos ==");
   const memos = await buyMeCoffee.getMemos();
   printMemos(memos);
 }
-
-  //We need to handle error
+//error handling
 main()
   .then(() => process.exit(0))
   .catch((error) => {
